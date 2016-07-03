4c37815fca0aa92561dc6285c07b47f78d9c3d3d
Tinkerspy <tinkerspy@myown.mailcan.com>
2016-07-03 10:13:22 +0200

    Minimal hello world web server example

---
+++ examples/hello_world/hello_world.ino
@@ -0,0 +1,26 @@
+#include <Automaton.h>
+#include <Atm_esp8266.h>
+
+// Minimal Hello World webserver
+
+Atm_esp8266_wifi wifi;
+Atm_esp8266_httpd_simple server( 80 );
+
+void setup() {
+  wifi.begin( "MySSID", "MyPASSWORD" )
+    .onChange( true, server, server.EVT_START )
+    .start();
+
+  server.begin()
+    .onRequest( "/", [] ( int idx, int v, int up ) {
+      server.send( 
+        "<!DOCTYPE html><html><body>"
+        "Hello world!"
+        "</body></html>"
+      );
+    });
+}
+
+void loop() {
+  automaton.run();
+}
