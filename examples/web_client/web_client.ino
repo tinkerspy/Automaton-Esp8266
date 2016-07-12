e5a293ff2db44015bc9bee47be73412815278613
Tinkerspy <tinkerspy@myown.mailcan.com>
2016-07-12 19:27:45 +0200

    More minimal example

--- examples/web_client/web_client.ino
+++ examples/web_client/web_client.ino
@@ -1,7 +1,7 @@
 #include <Automaton.h>
 #include <Atm_esp8266.h>
 
-Atm_esp8266_httpc_simple client;
+Atm_esp8266_httpc_simple client1, client2;
 Atm_esp8266_wifi wifi;
 Atm_button button1, button2;
 
@@ -10,43 +10,19 @@ Atm_button button1, button2;
 
 void setup() {
 
-  Serial.begin( 115200 );
-  Serial.println( "Start" );
-
   wifi.begin( "MySSID", "MyPASSWORD" ) 
-    .onChange( true, [] ( int idx, int v, int up  ) {
-      Serial.println( "Connected to Wifi");
-    })
-    .onChange( false, [] ( int idx, int v, int up  ) {
-      Serial.println( "Lost Wifi connection");      
-    })
     .led( LED_BUILTIN, true ) // Esp8266 built in led shows wifi status
     .start();  
 
-  client.begin( "172.22.22.220" )
-    .onFinish( [] ( int idx, int v, int up ) {
-      Serial.print( "Result: " );
-      Serial.println( client.responseCode() );
-      Serial.print( "[" );
-      Serial.print( client.headers() );
-      Serial.println( "]" );
-      Serial.print( "[" );
-      Serial.print( client.content() );
-      Serial.println( "]" );
-    });
+  client1.begin( "172.22.22.220" )
+    .get( "/on" );        
+  client2.begin( "172.22.22.220" )
+    .get( "/off" ); // 23 positions in a 1 night stand
         
   button1.begin( D2 )
-    .onPress( [] ( int idx, int v, int up ) {
-      client
-        .get( "/on" )
-        .start();
-    });
+    .onPress( client1, client1.EVT_START );
   button2.begin( D7 )
-    .onPress( [] ( int idx, int v, int up ) {
-      client
-        .get( "/off" ) // 23 positions in a one night stand
-        .start();
-    });
+    .onPress( client2, client2.EVT_START );
 
 }
 
